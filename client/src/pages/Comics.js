1b90803635657b4d215c0c2c781b7a8cbc47f2f3
Strontiumsun <izzyhall25@gmail.com>
2019-06-21 11:58:51 -0500

    added a new gallery component and got it to accept props

--- client/src/pages/Comics.js
+++ client/src/pages/Comics.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Gallery from "../components/Gallery"
+
+const images = [
+    "https://66.media.tumblr.com/492a1ec27de1e698e4681063274a0f96/tumblr_p9q2xg3NW51qjs67jo1_1280.png", "https://66.media.tumblr.com/91303b0c9e5f25db845510ddb8107c1b/tumblr_p9q2xg3NW51qjs67jo2_1280.png"
+]
 
 function Comics() {
     return (
-        <div><p>I'd like to have a section for my comic, and a section for other comics</p></div>
+        <div>
+            <Gallery images={images} />
+        </div>
     )
 }
 
